0caef7dca4d5d76f6221d7f6bfd630d26cf4ead0
lovoski <liuwei.contact@qq.com>
2023-09-23 11:07:05 +0800

    change google's name server to aliyun's

--- user/nettests.c
+++ user/nettests.c
@@ -233,7 +233,10 @@ dns()
   memset(ibuf, 0, N);
   
   // 8.8.8.8: google's name server
-  dst = (8 << 24) | (8 << 16) | (8 << 8) | (8 << 0);
+  // dst = (8 << 24) | (8 << 16) | (8 << 8) | (8 << 0);
+
+  // 223.5.5.5: aliyun's name server
+  dst = (223 << 24) | (5 << 16) | (5 << 8) | (5 << 0);
 
   if((fd = connect(dst, 10000, 53)) < 0){
     fprintf(2, "ping: connect() failed\n");
